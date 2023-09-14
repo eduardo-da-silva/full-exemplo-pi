5b1a054a0bd0d94e06329462c8f35c448868de13
Eduardo da Silva <eduardo.silva@gmail.com>
2023-09-14 16:47:18 -0300

    Add serializer to Movie with Image

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -1,10 +1,31 @@
 from rest_framework import serializers
-from .models import Genre
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
+from .models import Genre, Movie
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ("id", "name")
 
+class MovieSerializer(serializers.ModelSerializer):
+    cover_attachment_key = serializers.SlugRelatedField(
+        source="cover",
+        queryset=Image.objects.all(),       # pylint: disable=no-member
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    cover = ImageSerializer(required=False, read_only=True)
 
+    class Meta:
+        model = Movie
+        fields = ("id", "title", "year", "rating", "genre", "cover", "cover_attachment_key")
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    cover = ImageSerializer(required=False)
+
+    class Meta:
+        model = Movie
+        fields = ("id", "title", "year", "rating", "genre", "cover")
