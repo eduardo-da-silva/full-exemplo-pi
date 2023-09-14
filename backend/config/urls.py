df170a837b8673e9957f83b36c4203e044d0189d
Eduardo da Silva <eduardo.silva@gmail.com>
2023-09-14 16:30:18 -0300

    Add Pillow and routers to upload

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
+from uploader.router import router as uploader_router
 from movies.views import GenreViewSet
 
 router = DefaultRouter()
@@ -10,5 +13,7 @@ router.register("genres", GenreViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/media/", include(uploader_router.urls)),
     path("api/", include(router.urls)),
 ]
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
