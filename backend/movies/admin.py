7a3197f482c2c14b5424e9e32af19e32798b7745
Marco André Mendes <marcoandre@gmail.com>
2023-09-19 11:52:01 -0300

    admin feito e filmes cadastrados

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register all models here
+from .models import Genre, Movie
+
+admin.site.register(Genre)
+admin.site.register(Movie)
+
+
