c9f592765a9707eb970e58d4706566fa9bac141d
MiguelBochi <miguelbochibarro@gmail.com>
2023-10-09 15:23:31 -0300

    config

--- GrandeOrvalho_db_django/config/settings.py
+++ GrandeOrvalho_db_django/config/settings.py
@@ -59,9 +59,7 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "rest_framework.permissions.IsAuthenticated",
-    # ],
+    # "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated",],
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
--- GrandeOrvalho_db_django/config/urls.py
+++ GrandeOrvalho_db_django/config/urls.py
@@ -2,14 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from uploader.router import router as uploader_router
+from usuario.router import router as usuario_router
 
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from GrandeOrvalho.views import (
     AnimaisViewSet,
@@ -19,8 +21,6 @@ from GrandeOrvalho.views import (
     FuncionarioViewSet,
     RacaViewSet,
 )
-from uploader.router import router as uploader_router
-from usuario.router import router as usuario_router
 
 router = DefaultRouter()
 router.register(r"animais", AnimaisViewSet)
